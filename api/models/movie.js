e503fa9911a3d89914efc12f45976d5cec3b70d1
HudsonDanielle-FS <110405791+HudsonDanielle-FS@users.noreply.github.com>
2023-03-22 09:28:29 -0500

    post validation complete

--- api/models/movie.js
+++ api/models/movie.js
@@ -1,9 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const movieSchema = mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    title: String,
-    director: String
+  _id: mongoose.Schema.Types.ObjectId,
+  title: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('Movie', movieSchema);
+module.exports = mongoose.model("Movie", movieSchema);
--- api/routes/movies.js
+++ api/routes/movies.js
@@ -1,156 +1,181 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const { MongoDriverError } = require("mongodb");
+const mongoose = require("mongoose");
 const router = express.Router();
-const Movie = require('../models/movie');
+const Movie = require("../models/movie");
 
 //1. GET MovieS
-router.get('/', (req, res, next) => {
-    Movie.find()
-        .then(result => {
-            res.status(200).json({
-                message: "Get Movie",
-                movie: {
-                    title: result.title,
-                    director: result.director,
-                    id: result._id
-                },
-                metadata: {
-                    host: req.hostname,
-                    method: req.method
-                }           
-            });
-    })
-    .catch(err => {
-        console.error(err.message);
-        res.status(500).json({
-            error: {
-                message: err.message,
-                status: err.status
-            }
-        })        
-    });
-
-});
-
-router.get("/:movieId", (req, res, next) => {
-    const movieId = req.params.movieId;
-    Movie.findById(movieId)
-    .select("title _Id")
-    .exec()
-    .then(movie => {
-    //   if(!movie){
-    //     console.log(movie)
-    //     return res.status(404).json({
-    //       message: "movie not found"
-    //     })
-    //   }
+router.get("/", (req, res, next) => {
+  Movie.find()
+    .then((result) => {
       res.status(200).json({
-        message: "Movie Found"
-      })
+        message: "Get Movie",
+        movie: {
+          title: result.title,
+          director: result.director,
+          id: result._id,
+        },
+        metadata: {
+          host: req.hostname,
+          method: req.method,
+        },
+      });
     })
-    .catch(err => {
+    .catch((err) => {
+      console.error(err.message);
       res.status(500).json({
         error: {
-          message: err.message
-        }
-      })
-    })
-  });
-
-//2. POST - ADD NEW Movie
-router.post('/', (req, res, next) => {
-    const newMovie = new Movie({
-        _id: mongoose.Types.ObjectId(),
-        title: req.body.title,
-        director: req.body.director
+          message: err.message,
+          status: err.status,
+        },
+      });
     });
-    // Write to the db
-    newMovie.save()
-        .then(result => {
-            console.log(result);
-            res.status(200).json({
-                message: "Movie Saved",
-                movie: {
-                    title: result.title,
-                    director: result.director,
-                    id: result._id,
-                    metadata:{
-                        method: req.method,
-                        host: req.hostname
-                    }
-                }
-            })
-        })
-        .catch(err => {
-            console.error(err.message);
-            res.status(500).json({
-                error: {
-                    message: err.message,
-                    status: err.status
-                }
-            })        
-        });
 });
 
-router.delete("/:movieId", (req, res, next) => {
-    const movieId = req.params.movieId;
-    Movie.deleteOne({
-      _id: movieId,
+//2. Get Movies By Id
+router.get("/:movieId", (req, res, next) => {
+  const movieId = req.params.movieId;
+  Movie.findById(movieId)
+    .select("title _Id")
+    .exec()
+    .then((movie) => {
+      //   if(!movie){
+      //     console.log(movie)
+      //     return res.status(404).json({
+      //       message: "movie not found"
+      //     })
+      //   }
+      res.status(200).json({
+        message: "Movie Found",
+      });
     })
-      .exec()
-      .then((result) => {
-        res.status(200).json({
-          message: "Movie Deleted",
-          request: {
-            method: "GET",
-            url: "http://localhost:3000/directors/" + movieId,
-          },
-        });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          error: {
-            message: err.message,
-          },
-        });
+    .catch((err) => {
+      res.status(500).json({
+        error: {
+          message: err.message,
+        },
       });
-  });
+    });
+});
 
-  router.patch("/:movieId", (req, res, next) => {
-    const movieId = req.params.movieId;
-  
-    const updatedMovie = {
-      title: req.body.title,
-      director: req.body.director,
-    };
-  
-    Movie.updateOne(
-      { _id: movieId },
-      {
-        $set: updatedMovie,
+//3. POST - ADD NEW Movie
+router.post("/", (req, res, next) => {
+  Movie.find({
+    title: req.body.title,
+    director: req.body.director,
+  })
+    .exec()
+    .then((result) => {
+      console.log(result);
+      if (result.length > 0) {
+        return res.status(406).json({
+          message: "Movie is already cataloged",
+        });
       }
-    )
-      .then((result) => {
-        res.status(200).json({
-          message: "Updated Movie",
-          movie: {
-            title: result.title,
-            director: result.director,
-            id: result._id,
-          },
-          metadata: {
-            host: req.hostname,
-            method: req.method,
-          },
-        });
-      })
-      .catch((err) => {
-        res.status(500).json({
-          error: {
-            message: err.message,
-          },
-        });
+      const newMovie = new Movie({
+        _id: mongoose.Types.ObjectId(),
+        title: req.body.title,
+        director: req.body.director,
       });
-  });
+      // Write to the db
+      newMovie
+        .save()
+        .then((result) => {
+          console.log(result);
+          res.status(200).json({
+            message: "Movie Saved",
+            movie: {
+              title: result.title,
+              director: result.director,
+              id: result._id,
+              metadata: {
+                method: req.method,
+                host: req.hostname,
+              },
+            },
+          });
+        })
+        .catch((err) => {
+          console.error(err.message);
+          res.status(500).json({
+            error: {
+              message: err.message,
+              status: err.status,
+            },
+          });
+        });
+    })
+    .catch(err => {
+      console.error(error)
+      res.status(500).json({
+        error:{
+          message: "Unable to save movie with title: " + req.body.title
+        }
+      })
+    });
+});
+
+//4. Delete By Id
+router.delete("/:movieId", (req, res, next) => {
+  const movieId = req.params.movieId;
+  Movie.deleteOne({
+    _id: movieId,
+  })
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "Movie Deleted",
+        request: {
+          method: "GET",
+          url: "http://localhost:3000/directors/" + movieId,
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: {
+          message: err.message,
+        },
+      });
+    });
+});
+
+//5. Patch
+router.patch("/:movieId", (req, res, next) => {
+  const movieId = req.params.movieId;
+
+  const updatedMovie = {
+    title: req.body.title,
+    director: req.body.director,
+  };
+
+  Movie.updateOne(
+    { _id: movieId },
+    {
+      $set: updatedMovie,
+    }
+  )
+    .then((result) => {
+      res.status(200).json({
+        message: "Updated Movie",
+        movie: {
+          title: result.title,
+          director: result.director,
+          id: result._id,
+        },
+        metadata: {
+          host: req.hostname,
+          method: req.method,
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: {
+          message: err.message,
+        },
+      });
+    });
+});
 
 module.exports = router;
