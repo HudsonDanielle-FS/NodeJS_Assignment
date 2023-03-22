1d24de3a6d3540becdf2306072dc4cac566ff846
HudsonDanielle-FS <110405791+HudsonDanielle-FS@users.noreply.github.com>
2023-03-22 17:13:41 -0500

    methods updated with select and populate

--- api/routes/directors.js
+++ api/routes/directors.js
@@ -7,12 +7,18 @@ const Messages = require("../../messages/messages");
 
 //1. GET
 router.get("/", (req, res, next) => {
-  Director.find({})
+  Director.find()
+    .select("-__v")
+    .populate("movie", "title director")
     .exec()
     .then((result) => {
-      console.log(result);
       res.status(200).json({
-        message: "All directors returned",
+        message: Messages.all_directors_returned,
+        director: result,
+        metadata: {
+          method: req.method,
+          host: req.hostname,
+        },
       });
     })
     .catch((err) => {
@@ -27,25 +33,69 @@ router.get("/", (req, res, next) => {
 
 //2. POST
 router.post("/", (req, res, next) => {
-  res.json({
-    message: "Directors - POST",
-  });
+  Director.find({
+    movie: req.body.movie,
+    name: req.body.name,
+  })
+    .select("-__v")
+    .populate("movie", "title director")
+    .exec()
+    .then((result) => {
+      console.log(result);
+      if (result.length > 0) {
+        return res.status(406).json({
+          message: Messages.director_cataloged,
+        });
+      }
+      const newDirector = new Director({
+        _id: mongoose.Types.ObjectId(),
+        movie: req.body.movie,
+        name: req.body.name,
+      });
+      // Write to the db
+      newDirector
+        .save()
+        .then((result) => {
+          console.log(result);
+          res.status(200).json({
+            message: Messages.director_saved,
+            director: {
+              movie: result.movie,
+              name: result.name,
+              id: result._id,
+              metadata: {
+                method: req.method,
+                host: req.hostname,
+              },
+            },
+          });
+        })
+        .catch((err) => {
+          console.error(err.message);
+          res.status(500).json({
+            error: {
+              message: err.message,
+              status: err.status,
+            },
+          });
+        });
+    });
 });
 
 //3. GET by ID
 router.get("/:directorId", (req, res, next) => {
   const directorId = req.params.directorId;
   Director.findById(directorId)
-    .select("name _id")
+    .select("-__v")
     .populate("movie", "title director")
     .exec()
     .then((director) => {
-        if(!director){
-            console.log(director)
-            return res.status(404).json({
-                message: Messages.director_not_found
-            })
-        }
+      if (!director) {
+        console.log(director);
+        return res.status(404).json({
+          message: Messages.director_not_found,
+        });
+      }
       res.status(201).json({
         director: director,
       });
@@ -59,13 +109,47 @@ router.get("/:directorId", (req, res, next) => {
     });
 });
 
-//4. PATCH
+//4. PATCH by ID
 router.patch("/:directorId", (req, res, next) => {
   const directorId = req.params.directorId;
-  res.json({
-    message: "Directors - PATCH",
-    id: directorId,
-  });
+
+  const updatedDirector = {
+    movie: req.body.movie,
+    name: req.body.name,
+  };
+
+  Director.updateOne(
+    { _id: directorId },
+    {
+      $set: updatedDirector,
+    }
+  )
+    .select("-__v")
+    .populate("movie", "title director")
+    .exec()
+    .then((director) => {
+        if (!director) {
+            console.log(director);
+            return res.status(404).json({
+              message: Messages.director_not_found,
+            });
+          }
+      res.status(200).json({
+        message: Messages.director_updated,
+        director: director,
+        metadata: {
+          method: req.method,
+          host: req.hostname,
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: {
+          message: err.message,
+        },
+      });
+    });
 });
 
 //5. DELETE by Id
@@ -75,10 +159,18 @@ router.delete("/:directorId", (req, res, next) => {
   Director.deleteOne({
     _id: directorId,
   })
+    .select("-__v")
+    .populate("movie", "title director")
     .exec()
-    .then((result) => {
+    .then((director) => {
+        if (!director) {
+            console.log(director);
+            return res.status(404).json({
+              message: Messages.director_not_found,
+            });
+          }
       res.status(200).json({
-        message: "Director Deleted",
+        message: Messages.director_deleted,
         request: {
           method: "GET",
           url: "http://localhost:3000/directors/" + directorId,
--- messages/messages.js
+++ messages/messages.js
@@ -1,3 +1,8 @@
 module.exports = {
      director_not_found: "Director Not Found",
+     all_directors_returned: "All Directors Returned",
+     director_cataloged: "Director is already cataloged",
+     director_saved: "Director Saved",
+     director_updated: "Director Updated",
+     director_deleted: "Director Deleted"
 }
